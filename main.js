d4fe532b7f353e854569c240dff1c4554ac70cb2
Van Nguyen <vanthoai@fb.com>
2022-11-28 17:06:52 +0000

    added movement to character

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ const myGame = new Game({
         frameWidth: 16,
         frameHeight: 20,
       });
+      cursors = this.input.keyboard.createCursorKeys();
     },
 
     create() {
@@ -37,7 +38,71 @@ const myGame = new Game({
 
       player = this.physics.add.sprite(256, 256, "character");
       player.setCollideWorldBounds(true);
+
+      this.anims.create({
+        key: "right",
+        frames: this.anims.generateFrameNumbers("character", {
+          start: 7,
+          end: 9,
+        }),
+        frameRate: 10,
+        repeat: -1,
+      });
+
+      this.anims.create({
+        key: "left",
+        frames: this.anims.generateFrameNumbers("character", {
+          start: 4,
+          end: 6,
+        }),
+        frameRate: 10,
+        repeat: -1,
+      });
+
+      this.anims.create({
+        key: "up",
+        frames: this.anims.generateFrameNumbers("character", {
+          start: 10,
+          end: 12,
+        }),
+        frameRate: 10,
+        repeat: -1,
+      });
+
+      this.anims.create({
+        key: "down",
+        frames: this.anims.generateFrameNumbers("character", {
+          start: 1,
+          end: 3,
+        }),
+        frameRate: 10,
+        repeat: -1,
+      });
+
+      this.anims.create({
+        key: "idle",
+        frames: [{ key: "character", frame: 0 }],
+        frameRate: 20,
+      });
     },
-    update() {},
+    update() {
+      if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play("right", true);
+      } else if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play("left", true);
+      } else if (cursors.up.isDown) {
+        player.setVelocityY(-160);
+        player.anims.play("up", true);
+      } else if (cursors.down.isDown) {
+        player.setVelocityY(160);
+        player.anims.play("down", true);
+      } else {
+        player.setVelocityX(0);
+        player.setVelocityY(0);
+        player.anims.play("idle");
+      }
+    },
   },
 });
